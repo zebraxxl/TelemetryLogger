0cdf381dea2e0ee2a805021f0245a056b8a5c07d
zebraxxl <zebraxxl@mail.ru>
2015-05-08 15:36:37 +0300

    Markers lines

--- telemetry_logger/viewer/viewer_window.py
+++ telemetry_logger/viewer/viewer_window.py
@@ -17,8 +17,14 @@ class ViewerWindow:
         figure = pyplot.figure()
         axes, lines, labels = PLOT_DRAWERS[telemetry_name](telemetry_data, figure)
 
+        y_lim = 0.0
         for a in axes:
-            a.grid(True)
+            a.grid(True, color='0.35')
+            y_lim = max(y_lim, a.get_ylim()[1])
+
+        for m in self.markers:
+            pyplot.axvline(m[0], color='0.75')
+            pyplot.text(m[0], y_lim, m[1], rotation=-90, va='top')
 
         figure.tight_layout(pad=0)
         if labels is not None:
